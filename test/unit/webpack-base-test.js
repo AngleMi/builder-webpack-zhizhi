f7a0285c40599cd45a2c114c86636b3a8f56432e
AngleMi <minmin_zhi@126.com>
2020-07-21 11:10:57 +0800

    fix: 修改路径地址

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js');
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/template/src/index/index.js') > -1, true);
-        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/template/src/search/index.js') > -1, true);
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-zhizhi/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-zhizhi/test/smoke/template/src/search/index.js') > -1, true);
     });
 });
